3d0e430f2746dda5a35cc6f95b8e80e08a7368d2
Jarod <jarod@admins-MacBook-Pro.local>
2018-11-17 18:32:04 -0500

    Cache oblivious transpif

--- transpif.c
+++ transpif.c
@@ -3,36 +3,40 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-void printmat (int* matrix, int width);
+void printmat (int* matrix, int width, int height);
 
 int main (int argc, char**argv) {
-  if (argc != 3) {
+  if (argc != 4) {
     fprintf(stderr,
-	    "Wrong number of command-line arguments\nUsage: %s <Matrix width> <Block width>\n", argv[0]);
+	    "Wrong number of command-line arguments\nUsage: %s <Matrix width> <Matrix height> <Block width>\n", argv[0]);
     return -1;
   }
 
   int matwidth = atoi(argv[1]);
-  int blockwidth = atoi(argv[2]);
+  int matheight = atoi(argv[2]);
+  int blockwidth = atoi(argv[3]);
   if (blockwidth < 1) {
     printf("Invalid block width being replaced with 1\n");
     blockwidth = 1;
   }
 
-  int* matrix = malloc(matwidth*matwidth*sizeof(int));
+  int* matrix = malloc(matwidth*matheight*sizeof(int));
+  int* trans = malloc(matwidth*matheight*sizeof(int));
 
   for (int i = 0; i < matwidth*matwidth; i++) {
-    matrix[i] = rand();
-    //matrix[i] = i;
+    //matrix[i] = rand();
+    matrix[i] = i;
   }
 
   struct timeval starttime;
   gettimeofday(&starttime, NULL);
   
   int tmp;
-  //printmat(matrix, matwidth);
-  for (int i = 0; i < matwidth; i += blockwidth) {
-    for (int j = i; j < matwidth; j += blockwidth) {
+  printmat(matrix, matwidth, matheight);
+  for (int i = 0; i < matheight; i ++) {
+    for (int j = 0; j < matwidth; j ++) {
+      trans[j*matheight+i] = matrix[i*matwidth+j];
+      /*
       if (j == i) {
 	for (int k = 0; k < blockwidth; k++) {
 	  for (int l = k+1; l < blockwidth; l++) {
@@ -50,18 +54,19 @@ int main (int argc, char**argv) {
 	  }
 	}
       }
+      */
     }
   }
   struct timeval endtime;
   gettimeofday(&endtime, NULL);
   printf("Time: %ld microseconds\n", (endtime.tv_sec - starttime.tv_sec)*1000000 + endtime.tv_usec - starttime.tv_usec);
   
-  //printmat(matrix, matwidth);
+  printmat(trans, matheight, matwidth);
   free(matrix);
 }
 
-void printmat (int* matrix, int width) {
-  for (int i = 0; i < width*width; i++) {
+void printmat (int* matrix, int width, int height) {
+  for (int i = 0; i < width*height; i++) {
     printf ("%d\t",matrix[i]);
     if (i%width == width-1) {
       printf ("\n");
